24c754450e2ca42a639e6b2d569f6be3d9e5891d
Jessica Christine Gray <60930017+jcgray77@users.noreply.github.com>
2021-02-12 20:11:20 -0500

    Week4

---
+++ Week4/hashing.py
@@ -0,0 +1,11 @@
+int_val = 4
+str_val = 'PythonIsBest'
+flt_val = 24.56
+
+
+  
+print(hash(4))                                                                                                        
+
+print(hash(str_val))    
+
+print(hash(flt_val))  
